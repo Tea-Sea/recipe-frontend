d6dff1ad151304f95bbc3b7dd4abe066aa3bb8cc
Tea-Sea <thomcaspar@gmail.com>
2025-08-15 14:35:10 +0100

    Home page requests data from api on buttonclick. displays to users

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,21 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Recipe } from '../../types/recipe';
 import './Home.module.css';
 
 function Home() {
+  const apiUrl = process.env.REACT_APP_GO_API_URL;
+
+  // store fetched text here
+  const [recipe, setRecipe] = useState<Recipe| null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchRandomRecipe = async () => {
+    console.log('button click');
+
+    try {
+      const res = await fetch(`${apiUrl}/recipes/random`);
+      if (!res.ok) {
+        throw new Error(`API error: ${res.status}`);
+      }
+
+      const data = await res.json();
+      setRecipe(data); // customize based on response shape
+    } catch (err: any) {
+      setError(err.message);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="Home-Header">
+        <button className='Option-Button' onClick={fetchRandomRecipe}>
+          Give me a random recipe
+        </button>
+        {error && <p style={{ color: 'red' }}>Error: {error}</p>}
+         {recipe && (
+        <div>
+          <p><strong>ID:</strong> {recipe.id}</p>
+          <p><strong>Name:</strong> {recipe.name}</p>
+          <p><strong>Difficulty:</strong> {recipe.difficulty}</p>
+          <p><strong>Method:</strong> {recipe.method}</p>
+          <p><strong>Ingredients:</strong></p>
+          {/* <ul>
+            {recipe.ingredients.map((ingredient, i) => (
+              <li key={i}>{ingredient}</li>
+            ))}
+          </ul> */}
+        </div>
+      )}
       </header>
     </div>
   );
---
+++ src/types/recipe.ts
@@ -0,0 +1,7 @@
+export interface Recipe {
+  id: number;
+  name: string;
+  difficulty: number;
+  method: string;
+  ingredients: string[];
+}
