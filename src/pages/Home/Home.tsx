6e12be1c70aed4772a42f28b5c5630a0c8dd236f
Tea-Sea <thomcaspar@gmail.com>
2025-08-16 23:41:54 +0100

    Added recipeblock component. Implemented into recipebook

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { Recipe } from '../../types/recipe';
+import { Recipe } from '../../types/recipe.types';
 import './Home.module.css';
 
 function Home() {
--- src/pages/Recipes/Recipes.tsx
+++ src/pages/Recipes/Recipes.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import Recipe from '../../components/RecipeBlock'
+import { Recipe as RecipeType } from '../../types/recipe.types';
+
+const recipes: RecipeType[] = [
+  {
+    id: 1,
+    name: 'Spaghetti',
+    difficulty: 3,
+    ingredients: ['Pasta', 'Tomato Sauce', 'Garlic'],
+    method: 'Boil water, Cook pasta, Add sauce',
+  },
+  {
+    id: 2,
+    name: 'Pancakes',
+    difficulty: 1,
+    ingredients: ['Flour', 'Eggs', 'Milk'],
+    method: 'Mix ingredients Cook on pan',
+  }
+];
 
 function Recipes() {
   return (
     <div>
       <h3>THIS IS HOPEFULLY WHERE THE RECIPES WILL BE LISTED!!!!!!!</h3>
+      {recipes.map(recipe => (
+        <Recipe key={recipe.id} recipe={recipe} />
+      ))}
     </div>
   );
 }
