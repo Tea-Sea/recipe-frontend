298798ad970aeac5dc502407c1e2912b1f76b884
Tea-Sea <thomcaspar@gmail.com>
2025-08-19 09:40:58 +0100

    Cleaned up Repo a bit

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Recipes from '../pages/Recipes';
 import Error from '../pages/Error';
