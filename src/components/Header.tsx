e79e502689bc66ace0405a9d3c5066650f324faf
Tea-Sea <thomcaspar@gmail.com>
2025-10-10 18:04:37 +0100

    Touched up a few links functionality

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,23 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import HeaderButton from './HeaderButton';
 import Logo from './../../src/assets/TestLogo.svg?react';
 
+
+
 const Header: React.FC = () => {
+
+  const location = useLocation();
+
   return (
     <header className=' z-20 bg-blue-200 h-15'>
       <nav className='flex h-full items-end'>
-        <Link to="/" className='items-center'>
+        <Link
+        to="/"
+        className='items-center'
+        onClick={() => {
+          if (location.pathname === '/') window.location.reload();
+        }}
+        >
           <Logo role="img" aria-label="Cookbook Logo" width={60} height={60} viewBox="190 0 270 750" preserveAspectRatio="xMidYMid meet" className='hover:fill-gray-500 fill-gray-700 transition-all duration-300'></Logo>
         </Link>
         <ul className='flex flex-1 justify-center space-x-6 pb-2'>
